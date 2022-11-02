8488d65c9fb83de910bf3234312d549c58faf2dd
lucasmrdt <lucas.marandat@gmail.com>
2022-11-02 19:57:39 +0100

    mnist v1

--- diffusion_model/models/unet_model.py
+++ diffusion_model/models/unet_model.py
@@ -12,14 +12,17 @@ class Block(nn.Module):
     def __init__(self, in_ch, out_ch) -> None:
         super().__init__()
         self.conv1 = nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1)
-        self.act = nn.SiLU()
         self.conv2 = nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1)
+        self.relu = nn.ReLU()
+        self.bn = nn.BatchNorm2d(out_ch)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.act(x)
-        x = self.conv2(x)
-        x = self.act(x)
+        x = self.relu(x)
+        # x = self.bn(x)
+        # x = self.conv2(x)
+        # x = self.relu(x)
+        # x = self.bn(x)
         return x
 
 
--- diffusion_model/scheduler.py
+++ diffusion_model/scheduler.py
@@ -14,45 +14,43 @@ from .constants import device
 
 def get_linear_reparam_betas(nb_steps, min_alpha_bar=0, max_alpha_bar=1, max_beta=0.999):
     def f(t): return 1 - (1 - t)**2
-    ts = torch.linspace(max_alpha_bar, min_alpha_bar, nb_steps).to(device)
-    alpha_bar = torch.cat([torch.ones(), f(ts)
-    alpha_zero=max_alpha_bar
-    betas=1 - f(ts[1:])/f(ts[:-1])
-    betas=betas.clamp(0, max_beta)
+    ts = torch.linspace(max_alpha_bar, min_alpha_bar, nb_steps+1).to(device)
+    betas = 1 - f(ts[1:])/f(ts[:-1])
+    betas = betas.clamp(0, max_beta)
     return betas
 
 
 def get_cosine_betas(nb_steps, min_alpha_bar=0, max_alpha_bar=1, max_beta=0.999):
-    s=0.0008
+    s = 0.0008
     def f(t): return torch.cos((t + s)/(1 + s) * torch.pi/2)**2
-    ts=torch.linspace(min_alpha_bar, max_alpha_bar, nb_steps+1).to(device)
-    betas=1 - f(ts[1:])/f(ts[:-1])
-    betas=betas.clamp(0, max_beta)
+    ts = torch.linspace(min_alpha_bar, max_alpha_bar, nb_steps+1).to(device)
+    betas = 1 - f(ts[1:])/f(ts[:-1])
+    betas = betas.clamp(0, max_beta)
     return betas
 
 
 class Scheduler:
-    def __init__(self, input_dim, nb_steps, scheduler="linear-reparam", scheduler_kwargs={}):
-        self.nb_steps=nb_steps
+    def __init__(self, input_dim, nb_steps, schedule_type="linear-reparam", scheduler_kwargs={}):
+        self.nb_steps = nb_steps
 
         if isinstance(input_dim, int):
-            input_dim=(input_dim,)
-        self.input_dim=input_dim
+            input_dim = (input_dim,)
+        self.input_dim = input_dim
 
-        if scheduler == "linear-reparam":
-            betas=get_linear_reparam_betas(nb_steps, **scheduler_kwargs)
-        elif scheduler == "cosine":
-            betas=get_cosine_betas(nb_steps, **scheduler_kwargs)
+        if schedule_type == "linear-reparam":
+            betas = get_linear_reparam_betas(nb_steps, **scheduler_kwargs)
+        elif schedule_type == "cosine":
+            betas = get_cosine_betas(nb_steps, **scheduler_kwargs)
         else:
-            raise ValueError(f"Unknown scheduler {scheduler}")
+            raise ValueError(f"Unknown schedule_type {schedule_type}")
 
-        ds={f"d{i}": d for i, d in enumerate(input_dim)}
-        self.betas=repeat(betas, f"t -> t {' '.join(ds.keys())}", **ds)
+        ds = {f"d{i}": d for i, d in enumerate(input_dim)}
+        self.betas = repeat(betas, f"t -> t {' '.join(ds.keys())}", **ds)
 
-        self.alphas=1 - self.betas
-        self.sqrt_alpha=self.alphas.sqrt()
-        self.alphas_bar=self.alphas.cumprod(dim=0)
-        self.alphas_bar_prev=torch.cat(
+        self.alphas = 1 - self.betas
+        self.sqrt_alpha = self.alphas.sqrt()
+        self.alphas_bar = self.alphas.cumprod(dim=0)
+        self.alphas_bar_prev = torch.cat(
             [torch.ones(1, *input_dim).to(device), self.alphas_bar[:-1]])
-        self.sqrt_alphas_bar=self.alphas_bar.sqrt()
-        self.sqrt_one_minus_alphas_bar=(1 - self.alphas_bar).sqrt()
+        self.sqrt_alphas_bar = self.alphas_bar.sqrt()
+        self.sqrt_one_minus_alphas_bar = (1 - self.alphas_bar).sqrt()
