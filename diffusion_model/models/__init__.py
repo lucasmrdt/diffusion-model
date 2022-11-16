c5ce86657279a34426316dfafbccb280e26de987
Nicolas <nicolas.dunou@etu.emse.fr>
2022-11-16 15:24:25 +0100

    init

--- diffusion_model/models/__init__.py
+++ diffusion_model/models/__init__.py
@@ -1,9 +1,12 @@
 from .model_unet_v1 import Model_UNet_V1
 from .model_dense import model_dense
+from .model_Conv2D import model_Conv2D
 
 class ModelGetter:
     valid_choices = [
         "unet_v1",
+        "dense",
+        "conv"
     ]
     default = "unet_v1"
 
@@ -13,5 +16,7 @@ class ModelGetter:
             return Model_UNet_V1
         elif model == "dense":
             return model_dense
+        elif model == "conv":
+            return model_Conv2D
         else:
             raise ValueError(f"Unknown model: {model}")
